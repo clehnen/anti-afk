f70b02fc13c038566fc4ccdc1ea2a5e8821c09d4
CLehnen <chris@lehnen.dev>
2020-03-25 14:43:38 +0100

    update mouse path

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import robot from 'robotjs';
 import iohook from 'iohook';
 
-function shiftMouse() {
-  let {x, y} = robot.getMousePos();
+function wiggleMouse() {
+  const {x, y} = robot.getMousePos();
   console.log(x, y);
   robot.moveMouseSmooth(x + 100, y);
-  robot.moveMouseSmooth(x, y + 100);
   robot.moveMouseSmooth(x - 100, y);
-
-  robot.moveMouseSmooth(x, y - 100);
   robot.moveMouseSmooth(x, y);
 }
 
-const interval = setInterval(() => shiftMouse(), 3000);
-
+setInterval(() => wiggleMouse(), 3000);
 
 iohook.on('mousedown', (event) => {
   process.exit(0);
