8cb2fce587f969e2aefb0c639700a3b8ecf83b71
CLehnen <chris@lehnen.dev>
2020-03-25 18:23:14 +0100

    reformat

--- src/anti-afk.ts
+++ src/anti-afk.ts
@@ -1,6 +1,23 @@
 import {getMousePos, getScreenSize, moveMouseSmooth} from 'robotjs';
 import {on as registerMouseEvent, start as startListeners} from 'iohook';
 
+export function startAntiAFK(intervalPeriodMillis: number = 3000): void {
+  setupInterval(intervalPeriodMillis);
+  setupMousedownCancellation();
+}
+
+function setupInterval(intervalPeriodMillis: number): NodeJS.Timeout {
+  return setInterval(() => wiggleMouseLeftToRight(100), intervalPeriodMillis);
+}
+
+function setupMousedownCancellation(): void {
+  registerMouseEvent('mousedown', (_) => {
+    console.log('Shutting down');
+    process.exit(0);
+  });
+  startListeners();
+}
+
 function wiggleMouseLeftToRight(distance: number): void {
   const {x, y} = getMousePos();
   moveMouseSmooth(cropHorizontalToScreen(x + distance), y);
@@ -12,19 +29,4 @@ function cropHorizontalToScreen(x: number): number {
   return x <= 0 ? 0 : Math.min(x, getScreenSize().width);
 }
 
-function setupInterval(intervalPeriodMillis: number): void {
-  setInterval(() => wiggleMouseLeftToRight(100), intervalPeriodMillis);
-}
 
-function setupMousedownCancellation(): void {
-  registerMouseEvent('mousedown', (_) => {
-    console.log('Shutting down');
-    process.exit(0);
-  });
-  startListeners();
-}
-
-export function startAntiAFK(intervalPeriodMillis: number = 3000): void {
-  setupInterval(intervalPeriodMillis);
-  setupMousedownCancellation();
-}
