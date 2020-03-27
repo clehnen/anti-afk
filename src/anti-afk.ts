e153395d8348121512233a8b9406f42e51c6c1a8
CLehnen <chris@lehnen.dev>
2020-03-27 12:26:43 +0100

    removed unused methods

--- src/anti-afk.ts
+++ src/anti-afk.ts
@@ -1,33 +1,4 @@
 import {getMousePos, getScreenSize, moveMouseSmooth} from 'robotjs';
-import {on as registerIOEvent, start as startListeners} from 'iohook';
-
-export function startAntiAFK(
-    intervalPeriodMillis: number = 3000,
-    cancellationKeyCodes?: number[],
-): void {
-  setupInterval(intervalPeriodMillis);
-  setupCancellationEvents(cancellationKeyCodes);
-}
-
-function setupInterval(intervalPeriodMillis: number): NodeJS.Timeout {
-  return setInterval(() => wiggleMouseLeftToRight(100), intervalPeriodMillis);
-}
-
-function setupCancellationEvents(keyCodes?: number[]): void {
-  registerIOEvent('mousedown', () => {
-    shutdown();
-  });
-
-  registerIOEvent('keydown', (key) => {
-    // close on ESC button press
-    if (key.rawcode == 27 || (keyCodes?.includes(key.rawcode))) {
-      shutdown();
-    }
-
-  });
-
-  startListeners();
-}
 
 export function wiggleMouseLeftToRight(distance: number): void {
   type Point = { x: number, y: number };
@@ -46,10 +17,3 @@ export function wiggleMouseLeftToRight(distance: number): void {
 function limitToScreenSize(x: number): number {
   return x <= 0 ? 0 : Math.min(x, getScreenSize().width);
 }
-
-function shutdown(): void {
-  console.log('Shutting down');
-  process.exit(0);
-}
-
-
