0ac33b670d95fc323f1259432ef68ec456ce9099
CLehnen <chris@lehnen.dev>
2021-02-15 10:36:11 +0100

    change start command

--- out/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var auto_mode_1 = require("./auto-mode");
-var argparse_1 = require("./argparse");
-// startAntiAFK();
-console.log('Started Anti-AFK');
-var args = argparse_1.parseArguments();
-auto_mode_1.AntiAfkAuto.fromArguments(args).start();
-//# sourceMappingURL=main.js.map
